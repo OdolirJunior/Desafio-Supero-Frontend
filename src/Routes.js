6f5f1028e4e887c18cc6e2c3a54ec756a58b950e
panthrotech@gmail.com <panthrotech@gmail.com>
2019-11-24 14:29:57 -0300

    Ajuste

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Login from "./Containers/Login/Login";
 class Routes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isLogged: false };
+    this.state = { isLogged: true };
   }
 
   render() {

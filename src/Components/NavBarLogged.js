b97374b5d2218f10f0f4b3ce7bcf12d1f54dcbff
Gustavo <gustavopcassol@gmail.com>
2019-12-01 22:08:53 -0300

    adicionado botao logout

--- src/Components/NavBarLogged.js
+++ src/Components/NavBarLogged.js
@@ -8,11 +8,25 @@ class NavBarLogged extends React.Component {
       return myName[0].split("=")[1];
     }
   }
+
+  logout() {
+      let cookie = (document.cookie) && document.cookie.split("; ");
+
+      if (cookie[2]) {
+          cookie[2] = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          document.cookie = cookie
+      }
+      console.log(cookie)
+  }
+
   render() {
     return (
       <div className="topnav">
         <h2 className="textoprincipal">To-dos</h2>
-        <div className="login-container">{this.myName()}</div>
+        <div className="login-container">
+            {this.myName()}
+            <button className="logout-button" onClick={this.logout}>Logout</button>
+        </div>
       </div>
     );
   }
